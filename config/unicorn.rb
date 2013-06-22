42523085c14f0536d25c7725dadbf5d7210b71bf
Billy Reisinger <billy.reisinger@gmail.com>
2013-06-22 00:22:15 -0500

    getting ready to serve

---
+++ config/unicorn.rb
@@ -0,0 +1,11 @@
+@dir = "/opt/garagio/"
+
+worker_processes 2
+working_directory @dir
+
+timeout 30
+
+pid "#{@dir}tmp/pids/unicorn.pid"
+
+stderr_path "#{@dir}log/unicorn.stderr.log"
+stdout_path "#{@dir}log/unicorn.stdout.log"
