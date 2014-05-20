f3e07e89f0bb846a89a41bb1a37c20aa67ffa8f6
Billy Reisinger <billy.reisinger@gmail.com>
2014-05-19 22:40:34 -0500

    friendlier config loading

--- lib/garagio/garagio_config.rb
+++ lib/garagio/garagio_config.rb
@@ -5,6 +5,10 @@ class GaragioConfig
   end
 
   def self.config
-    @config ||= YAML.load_file(File.expand_path("../../../config/config.yml", __FILE__))
+    @config ||= begin
+      YAML.load_file(File.expand_path("../../../config/config.yml", __FILE__))
+    rescue Errno::ENOENT 
+      {}
+    end
   end
 end
